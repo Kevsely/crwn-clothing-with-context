9fbe0dc018c22749147d74793dd70e980fc2b848
Kevsely <elegis.sossou@yahoo.com>
2022-12-01 15:42:17 +0100

    Build the CartIcon cmp and added it the the navigation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+
 import { UserContext } from '../../contexts/user.context'
 import { ReactComponent as CwrnLogo } from '../../assets/crown.svg'
 
@@ -34,6 +36,7 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
 			</div>
 			<Outlet />
